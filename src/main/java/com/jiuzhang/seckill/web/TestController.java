a176ff8f411e91a873c490d933e50bafe9441316
Oliver Zhang <yzhang58@email.wm.edu>
2023-01-16 19:02:20 -0800

    Added test controllers for Sentinel to test throttling

---
+++ src/main/java/com/jiuzhang/seckill/web/TestController.java
@@ -0,0 +1,64 @@
+package com.jiuzhang.seckill.web;
+
+import com.alibaba.csp.sentinel.Entry;
+import com.alibaba.csp.sentinel.SphU;
+import com.alibaba.csp.sentinel.slots.block.BlockException;
+import com.alibaba.csp.sentinel.slots.block.RuleConstant;
+import com.alibaba.csp.sentinel.slots.block.flow.FlowRule;
+import com.alibaba.csp.sentinel.slots.block.flow.FlowRuleManager;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.annotation.PostConstruct;
+import java.util.ArrayList;
+import java.util.List;
+
+@Slf4j
+@Controller
+public class TestController {
+
+    @ResponseBody
+    @RequestMapping("hello")
+    public String hello() {
+        String result;
+
+        try (Entry entry = SphU.entry("HelloResource")) {
+            // protected logic
+            result = "Hello Sentinel!";
+            return result;
+        } catch (BlockException ex) {
+            // block resource access, throttle or downgrade
+            log.error(ex.toString());
+            result = "System busy, please try later";
+            return result;
+        }
+    }
+
+    /**
+     * Define throttling rules
+     * 1. create a set to store rules of throttling
+     * 2. create throttling rules
+     * 3. put rules into the set
+     * 4. load the rules
+     * @PostContruct means to execute after constructor finishes
+     */
+    @PostConstruct
+    public void initFlowRules() {
+        // 1. create a set to store rules of throttling
+        List<FlowRule> rules = new ArrayList<>();
+        // 2. create throttling rules
+        FlowRule rule = new FlowRule();
+        // set up resource, so that Sentinel will be effective to the corresponding resource
+        rule.setResource("HelloResource");
+        // define throttling rule types (type is QPS)
+        rule.setGrade(RuleConstant.FLOW_GRADE_QPS);
+        // define QPS = query per second to be 2
+        rule.setCount(2);
+        // 3. put rules into the set
+        rules.add(rule);
+        // load the rules
+        FlowRuleManager.loadRules(rules);
+    }
+}
